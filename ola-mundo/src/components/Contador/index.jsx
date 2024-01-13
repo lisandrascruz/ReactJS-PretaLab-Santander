0d58f68b4df98e63a9a9ac6e9095bc2380c8ec01
Lisandra Cruz <lisansouza@gmail.com>
2024-01-13 09:58:32 -0300

    Funções do contador

--- ola-mundo/src/components/Contador/index.jsx
+++ ola-mundo/src/components/Contador/index.jsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react";
 import Botao from "../Botao/index";
 import "./styles.css";
 
 const Contador = () => {
+  const [valor, setValor] = useState(0);
+  const [qtdCliques, setQtdCliques] = useState(0);
+
+  const incrementar = () => {
+    setValor((valorAnterior) => valorAnterior + 1);
+  };
+
+  const decrementar = () => {
+    setValor((valorAnterior) => valorAnterior - 1);
+  };
+
   return (
     <section className="contador-section">
       <h1>Contador</h1>
+
       <main className="contador-main">
-        <Botao texto="+" />
-        <h2 className="counter-value">0</h2>
-        <Botao texto="-" />
+        <Botao texto="+" acao={incrementar} />
+        <h2 className="counter-value">{valor}</h2>
+        <Botao texto="-" acao={decrementar} />
       </main>
     </section>
   );
