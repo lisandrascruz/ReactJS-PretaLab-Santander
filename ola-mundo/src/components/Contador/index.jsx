1661809d878f8bdd7eec5ad412f015df679aab11
Lisandra Cruz <lisansouza@gmail.com>
2024-01-13 11:32:08 -0300

    Contador

--- ola-mundo/src/components/Contador/index.jsx
+++ ola-mundo/src/components/Contador/index.jsx
@@ -1,14 +1,29 @@
-import Botao from "../Botao/index";
+import { useState } from "react";
+import Botao from "../Botao";
 import "./styles.css";
 
 const Contador = () => {
+  const [valor, setValor] = useState(0);
+
+  const incrementar = () => {
+    // setValor(valor + 1);
+    // boa pratica:
+    setValor((valorAnterior) => valorAnterior + 1);
+  };
+
+  const decrementar = () => {
+    // setValor(valor - 1);
+    // boa pratica:
+    setValor((valorAnterior) => valorAnterior - 1);
+  };
+
   return (
     <section className="contador-section">
       <h1>Contador</h1>
       <main className="contador-main">
-        <Botao texto="+" />
-        <h2 className="counter-value">0</h2>
-        <Botao texto="-" />
+        <Botao texto="+" acao={incrementar} />
+        <h2 className="contador-value">{valor}</h2>
+        <Botao texto="-" acao={decrementar} />
       </main>
     </section>
   );
