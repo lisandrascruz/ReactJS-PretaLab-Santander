bf217618c48ed4b895ec55d9be777889d8ea497b
Lisandra Cruz <lisansouza@gmail.com>
2024-01-25 20:09:44 -0300

    conclusao da pagina de endereco

--- ola-mundo/src/pages/EnderecoPage/index.jsx
+++ ola-mundo/src/pages/EnderecoPage/index.jsx
@@ -4,15 +4,38 @@ import Link from "@mui/material/Link";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import axios from "axios";
 import { useState } from "react";
 
 const EnderecoPage = () => {
   const [cep, setCep] = useState("");
-  const [rua, setRua] = useState("Rua da Amora");
-  const [cidade, setCidade] = useState("Canjiquinha");
-  const [estado, setEstado] = useState("Pamonha");
-  const [latitude, setLatitude] = useState("-12423");
-  const [longitude, setLongitude] = useState("1231232312");
+  const [endereco, setEndereco] = useState({
+    rua: "",
+    cidade: "",
+    estado: "",
+    latitude: "",
+    longitude: "",
+  });
+
+  const consultarCEP = async () => {
+    try {
+      const resposta = await axios.get(
+        `https://brasilapi.com.br/api/cep/v2/${cep}`
+      );
+
+      const enderecoResposta = resposta.data;
+
+      setEndereco({
+        rua: enderecoResposta.street,
+        cidade: enderecoResposta.city,
+        estado: enderecoResposta.state,
+        latitude: enderecoResposta.location.coordinates.latitude,
+        longitude: enderecoResposta.location.coordinates.longitude,
+      });
+    } catch (erro) {
+      console.erro("Um erro aconteceu ao buscar o CEP");
+    }
+  };
 
   return (
     <Box
@@ -43,15 +66,27 @@ const EnderecoPage = () => {
           value={cep}
           onChange={(event) => setCep(event.target.value)}
         />
-        <Button variant="contained">Buscar</Button>
+        <Button variant="contained" onClick={() => consultarCEP()}>
+          Buscar
+        </Button>
       </Stack>
 
       <Stack width="70vh" gap={2} mb={4}>
-        <TextField label="Rua" fullWidth readOnly value={rua} />
-        <TextField label="Cidade" fullWidth readOnly value={cidade} />
-        <TextField label="Estado" fullWidth readOnly value={estado} />
-        <TextField label="Latitude" fullWidth readOnly value={latitude} />
-        <TextField label="Longitude" fullWidth readOnly value={longitude} />
+        <TextField label="Rua" fullWidth readOnly value={endereco.rua} />
+        <TextField label="Cidade" fullWidth readOnly value={endereco.cidade} />
+        <TextField label="Estado" fullWidth readOnly value={endereco.estado} />
+        <TextField
+          label="Latitude"
+          fullWidth
+          readOnly
+          value={endereco.latitude}
+        />
+        <TextField
+          label="Longitude"
+          fullWidth
+          readOnly
+          value={endereco.longitude}
+        />
       </Stack>
     </Box>
   );
