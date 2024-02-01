238724cf8ee5952fcf0e5fc46a8804f32b023a7a
Lisandra Cruz <lisansouza@gmail.com>
2024-02-01 18:21:15 -0300

    codigo da aula 30/01/2024

---
+++ ola-mundo/src/js/funcoes.js
@@ -0,0 +1,21 @@
+// A empresa Limão com Mel tem um quadro de 200 pessoas desenvolvedoras
+// e ela decidiu dar aumento a todas seguindo os seguintes
+// critérios: dev jr ganha 20% de aumento,
+// dev pleno ganha 12% de aumento e o dev senior ganha 5% de aumento.
+
+export const concederAumento = (cargo, salario) => {
+  let percentualAumento = 0;
+
+  if (cargo === "dev junior") {
+    percentualAumento = 0.2;
+  } else if (cargo === "dev pleno") {
+    percentualAumento = 0.12;
+  } else if (cargo === "dev senior") {
+    percentualAumento = 0.05;
+  }
+
+  const valorAumento = salario * percentualAumento;
+  const salarioAtualizado = salario + valorAumento;
+
+  return salarioAtualizado;
+};
---
+++ ola-mundo/src/js/funcoes.test.js
@@ -0,0 +1,28 @@
+import { describe, expect, it } from "vitest";
+import { concederAumento } from "./funcoes";
+
+// test("1 forma de crir teste", () => {
+//   escrever o teste aqui
+// });
+
+describe("concederAumento", () => {
+  it("deve calcular o aumento de um dev junior corretamente", () => {
+    const salario = concederAumento("dev junior", 1000);
+    expect(salario).toBe(1200);
+  });
+
+  it("deve calcular o aumento de um dev pleno corretamente", () => {
+    const salario = concederAumento("dev pleno", 1000);
+    expect(salario).toBe(1120);
+  });
+
+  it("deve calcular o aumento de um dev senior corretamente", () => {
+    const salario = concederAumento("dev senior", 1000);
+    expect(salario).toBe(1050);
+  });
+
+  it("deve calcular corretamente o aumento concedido a um funcionario que não seja dev ", () => {
+    const salario = concederAumento("analista de banco de dados", 1000);
+    expect(salario).toBe(1000);
+  });
+});
